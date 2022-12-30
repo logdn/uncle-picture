bed775fc33bcc5c47827ce9ff93b4b702670671d
Logan Dumaine <36392368+logdn@users.noreply.github.com>
2022-12-30 14:26:42 -0700

    Update lambda_function.py

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -12,9 +12,10 @@ ROOT = Path(__file__).resolve().parents[0]
 
 def get_photo(photo_to_post):
     s3_client = boto3.client('s3')
+    photo_bucket = os.getenv("AWS_PHOTO_BUCKET")
     # Download the object from the S3 bucket
     photo_to_save = '/tmp/' + photo_to_post
-    s3_client.download_file('bot-bucket-uncle-picture', photo_to_post, photo_to_save)
+    s3_client.download_file(photo_bucket, photo_to_post, photo_to_save)
     # Return the path to the downloaded file
     return photo_to_save
 
@@ -22,10 +23,11 @@ def get_photo(photo_to_post):
 def tweet_photo():    
     # Get CSV of posted photos
     s3_resource = boto3.resource('s3')
-    bucket = s3_resource.Bucket('bot-bucket-csv')
+    csv_bucket = os.getenv("AWS_CSV_BUCKET")
+    bucket = s3_resource.Bucket(csv_bucket)
     key = 'posted_photos.csv'
     tweets_file = '/tmp/posted_photos.csv'
-    s3_resource.Bucket('bot-bucket-csv').download_file(key,tweets_file)
+    s3_resource.Bucket(csv_bucket).download_file(key,tweets_file)
     df = pd.read_csv(tweets_file,header=0)
     
     consumer_key = os.getenv("CONSUMER_KEY")
@@ -35,7 +37,8 @@ def tweet_photo():
     
     # Get list of photos in S3 bucket
     s3_client = boto3.client('s3')
-    objects = s3_client.list_objects(Bucket='bot-bucket-uncle-picture')
+    photo_bucket = os.getenv("AWS_PHOTO_BUCKET")
+    objects = s3_client.list_objects(Bucket=photo_bucket)
     
     # Authenticate Twitter
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -44,20 +47,27 @@ def tweet_photo():
     
     # Loop through objects and post to Twitter if not already posted
     status = False
-    for obj in objects['Contents']:
-        if obj['Key'] not in df['Photos'].to_numpy():
+    for x in range(len(objects['Contents'])):
+        # Select random photo
+        df_sample = pd.DataFrame.from_dict(objects['Contents']).sample()
+        random_key = df_sample['Key'].values[0]
+        # Tweet photo if we haven't tweeted it yet
+        if random_key not in df['Photos'].to_numpy():
             # Get photo
-            tweet_photo = get_photo(obj['Key'])
+            tweet_photo = get_photo(random_key)
             # Post photo to Twitter
             media = api.media_upload(tweet_photo)
             tweet = ""
             post_result = api.update_status(status=tweet, media_ids=[media.media_id])
             # Add photo to list of posted photos
-            df_local = pd.DataFrame([obj['Key']], columns =['Photos'])
+            df_local = pd.DataFrame([random_key], columns =['Photos'])
             df = pd.concat([df, df_local], ignore_index = True)
             status = True
             break
-    
+        # just to be explicit
+        else:
+            continue
+        
     if status == True:
         # Save updated list of posted photos to temp CSV file
         df.to_csv(tweets_file,index=False)
@@ -73,7 +83,7 @@ def tweet_photo():
         df = pd.concat([df, df_local], ignore_index = True)
         df.to_csv(tweets_file,index=False)
         bucket.upload_file(tweets_file, key)
-    
+        
 def lambda_handler(event, context):
     print("Tweet photo")
     tweet_photo()
